2656f357ca0f304fea8e597054cfa9437cb3d442
wblanken <blankenship.will@gmail.com>
2014-10-20 17:39:57 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZipUtil
+{
+    class Program
+    {
+        private static bool create;
+        private static string name;
+        private static string target;
+
+        static void Main(string[] args)
+        {
+            if (args[1] == "true")
+                create = true;
+            else
+                create = false;
+
+            name = args[2];
+            target = args[3];
+
+
+            if (create)
+                ZipFile.CreateFromDirectory(target, name, CompressionLevel.Optimal, false);
+            else
+                ZipFile.ExtractToDirectory(name, target);
+        }
+    }
+}
